da564874d3e5bf8c46f00bef2220b6b2ab875bd7
Maritza Ruiz Ospina <maritzaruizospina@gmail.com>
2023-09-18 21:40:56 -0500

    Inicialización proyecto, contexto y conexión a base de datos, configuracion carpetas API[

---
+++ ApiPeliculas/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiPeliculas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPeliculas.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
